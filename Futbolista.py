e595bd0d3dc783a6c138a3dbf9cfe2ee4e426d56
KatherinePuentes <141962369+KatherinePuentes@users.noreply.github.com>
2023-10-16 03:25:08 +0000

    ultimo commit

--- Futbolista.py
+++ Futbolista.py
@@ -11,3 +11,24 @@ class Futbolista(Persona, Deportista):
         self._tarjetasRojas= tarjetasRojas
         self._piernaHabil= piernaHabil
         Futbolista.listaFutbolistas.append(self)
+    
+    def getGolesMarcados(self):
+        return self._golesMarcados
+
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados= golesMarcados
+
+    def getTargetasRojas(self):
+        return self._tarjetasRojas
+
+    def setTargetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas= tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil= piernaHabil
+
+    def __str__(self):
+        return f"Mi nombre es {getNombre()} soy profesional en el deporte {getDeporte()} Tengo {getEdad()} años de edad y llevo {getAñosPracticando()} años en el deporte"
